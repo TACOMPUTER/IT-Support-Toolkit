61052176b596cfcdba33a409a1eb7c31d1b366d6
TACOMPUTER <mail.tacomputer@gmail.com>
2026-06-23 16:51:25 +0700

    Update IT_Github.ps1

--- cmd-Powershell/IT_Github.ps1
+++ cmd-Powershell/IT_Github.ps1
@@ -191,7 +191,7 @@ function Run-IT-xxx {
 }
 
 function Show-Menu-IT {
-    # Clear-Host
+    Clear-Host
     $script:ReportLines = New-Object System.Collections.Generic.List[string]
 
     Write-Host ("+" * 75) -ForegroundColor DarkGray
